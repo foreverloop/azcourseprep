c51d262f6bc4ed4886f981073549d766dc0da9df
foreverloop <cjones5709@gmail.com>
2019-09-13 18:10:54 +0100

    integration prep example R

---
+++ integration_ex.R
@@ -0,0 +1,9 @@
+#similar to final example of 0 SEN in 5 tests
+pbinom(q=0,size=5,p=0.2)
+
+help(polyroot)
+
+equation_coefficients <- c(0,0,3,-1)
+polyroot(z=equation_coefficients)
+
+#only the real components of these are our answers, so 0 and 3
