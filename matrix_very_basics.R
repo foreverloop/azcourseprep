4709693695cc52efbe2d262b5223276960323e7b
foreverloop <cjones5709@gmail.com>
2019-09-14 23:47:37 +0100

    basic matrix functions in R

---
+++ matrix_very_basics.R
@@ -0,0 +1,16 @@
+library(MASS)
+help(solve)
+
+# basic_m <- c(2,1,3,4)
+# dim(basic_m) = c(2,2)
+# 
+# basic_m
+# 
+# solve(basic_m)
+
+basic_m <- c(2,1,3,4)
+dim(basic_m) = c(2,2)
+
+basic_m
+
+solve(basic_m)
