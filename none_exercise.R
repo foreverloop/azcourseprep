55c49c28a8c5da58e8d81d3339bdcdf60eafba53
foreverloop <cjones5709@gmail.com>
2019-09-15 11:49:24 +0100

    begin R tutorials

---
+++ none_exercise.R
@@ -0,0 +1,15 @@
+#code which is not explicitly an exercise...but may be useful to have in reference
+
+#trig functions are in the base of R
+cos(pi)
+sin(3*pi/2)
+tan(pi/6)
+
+#exponential (e^x) and log(x) are in the base of R
+exp(1)
+log(100,base=10) #number to raise for, base, return value is log of given arguments
+
+#modulus is two percentage symbols (returns remainder after division)
+16%%3
+10%%5
+
